cf2119c463f9e17d905f6578b5fda16f69eaabdc
Richart Escobedo <r.escobedo@ulasalle.edu.pe>
2023-06-15 20:05:44 -0500

    ahorcado html+nodeJS only get questions

---
+++ juego_backend/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+var cors = require('cors');
+const readline = require('readline');
+
+const app = express()
+
+app.use(express.static('pub'))
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(3000, () => {
+	console.log("Escuchando en: http://localhost:3000")
+});
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'menu.html'))
+})
+app.get('/jugar', (request, response) => {
+	//response.sendFile(path.resolve(__dirname, 'juego.html'))
+	const titulo = fs.readFileSync(path.resolve(__dirname, 'preguntas', 'programacion.txt'), 'utf8').split('\n')[0];
+	
+	fs.readFile(path.resolve(__dirname, 'juego.html'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      response.status(500).send('Error al leer el archivo HTML.');
+      return;
+    }
+
+    const html = data.replace('{{ titulo }}', titulo);
+    response.send(html);
+	});
+})
+
+app.get('/pedir', (request, response) => {
+	const preguntas = [];
+  
+	const readInterface = readline.createInterface({
+	  input: fs.createReadStream(path.resolve(__dirname, 'preguntas', 'programacion.txt')),
+	  crlfDelay: Infinity
+	});
+  
+	const readLines = async () => {
+	  for await (const line of readInterface) {
+		const [pregunta, respuesta] = line.split('|');
+		preguntas.push({ pregunta, respuesta });
+	  }
+  
+	  response.json(preguntas);
+	};
+  
+	readLines().catch((error) => {
+	  console.error(error);
+	  response.status(500).send('Error al leer el archivo de preguntas.');
+	});
+  });
